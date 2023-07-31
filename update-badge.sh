ca6b1a2ffab90d632c404c14ede38d6463d149b7
FireIsGood <fireisadequate@gmail.com>
2023-07-31 04:28:28 -0700

    Change update script to be a random number

--- update-badge.sh
+++ update-badge.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # DL=`curl "https://api.jsonbin.io/v3/qs/64c7841fb89b1e2299c8d98e" | jq ".record.downloads"`
-DL=`$RANDOM`
+DL=`curl -s "https://www.random.org/integers/?num=1&min=1&max=100&col=5&base=10&format=plain&rnd=new" | tr -d '[:blank:]'`
+
+echo "Replacing number with $DL"
 
 sed -i "/^\!\[Static Badge\]/c![Static Badge](https://img.shields.io/badge/label-$DL-7c3aed?style=plastic&logo=obsidian)" README.md
