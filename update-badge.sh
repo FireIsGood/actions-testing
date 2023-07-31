1602189e7be6b9681e55380fc3cd1f94b137b2ab
FireIsGood <fireisadequate@gmail.com>
2023-07-31 04:22:23 -0700

    Modify script to paste a random string

--- update-badge.sh
+++ update-badge.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-DL=`curl "https://api.jsonbin.io/v3/qs/64c7841fb89b1e2299c8d98e" | jq ".record.downloads"`
+# DL=`curl "https://api.jsonbin.io/v3/qs/64c7841fb89b1e2299c8d98e" | jq ".record.downloads"`
+DL=`$RANDOM`
 
 sed -i "/^\!\[Static Badge\]/c![Static Badge](https://img.shields.io/badge/label-$DL-7c3aed?style=plastic&logo=obsidian)" README.md
