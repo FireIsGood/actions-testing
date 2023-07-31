9eb5f610e19415469ea4a9f651ed8b1a4dcd36af
FireIsGood <fireisadequate@gmail.com>
2023-07-31 03:54:33 -0700

    Add script

---
+++ update-badge.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DL=`curl "https://api.jsonbin.io/v3/qs/64c7841fb89b1e2299c8d98e" | jq ".record.downloads"`
+
+sed -i "/^\!\[Static Badge\]/c![Static Badge](https://img.shields.io/badge/label-$DL-7c3aed?style=plastic&logo=obsidian)" README.md
